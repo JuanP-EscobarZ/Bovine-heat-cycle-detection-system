7afadc1f25eee857f490a9047ea413c05fe79d42
JuanP-EscobarZ <62301674+JuanP-EscobarZ@users.noreply.github.com>
2021-10-28 18:22:28 -0500

    V1.0

--- Code/LoRa Duplex.ino
+++ Code/LoRa Duplex.ino
@@ -1,16 +1,3 @@
-/*
-  LoRa Duplex communication
-
-  Sends a message every half second, and polls continually
-  for new incoming messages. Implements a one-byte addressing scheme,
-  with 0xFF as the broadcast address.
-
-  Uses readString() from Stream class to read payload. The Stream class'
-  timeout may affect other functuons, like the radio's callback. For an
-
-  created 28 April 2017
-  by Tom Igoe
-*/
 #include <SPI.h>              // include libraries
 #include <LoRa.h>
 
